373991942593e1498d6762f005e315788ce99546
rahulnk09 <66001514+rahulnk09@users.noreply.github.com>
2026-08-18 10:47:02 +0530

    Add: sql-create-table - submission-2

---
+++ SQL/sql-create-table/submission-2.sql
@@ -0,0 +1,16 @@
+Create TABLE videos (
+    id INTEGER,
+    name TEXT,
+    created_at date,
+    published boolean
+);
+
+
+-- Do not modify below this line --
+SELECT table_name, column_name, data_type
+FROM information_schema.columns
+WHERE table_name = 'videos';
+
+
+
+
